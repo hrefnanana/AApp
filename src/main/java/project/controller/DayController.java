a54d0df29feafed2eaee1f59f05437e620a21872
Hrefna <hrefnanana@gmail.com>
2015-10-28 19:11:55 +0000

    prototype?

--- src/main/java/project/controller/DayController.java
+++ src/main/java/project/controller/DayController.java
@@ -1,5 +1,90 @@
 package project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import project.persistence.entities.Day;
+import project.persistence.repositories.DayRepository;
+import project.service.DayService;
+
+@Controller
 public class DayController {
 
+    // Instance Variables
+    DayService dayService;
+
+    // Dependency Injection
+    @Autowired
+    public DayController(DayService dayService) {
+        this.dayService = dayService;
+    }
+
+    // Method that returns the correct view for the URL /postit
+    // This handles the GET request for this URL
+    // Notice the `method = RequestMethod.GET` part
+    @RequestMapping(value = "/day", method = RequestMethod.GET)
+    public String dayViewGet(Model model){
+
+        // Add a new Postit Note to the model for the form
+        // If you look at the form in PostitNotes.jsp, you can see that we
+        // reference this attribute there by the name `postitNote`.
+        model.addAttribute("day",new Day());
+
+        // Here we get all the Postit Notes (in a reverse order) and add them to the model
+        model.addAttribute("day",dayService.findAllByOrderByIdDesc());
+
+        // Return the view
+        return "day/Days";
+    }
+
+    // Method that receives the POST request on the URL /postit
+    // and receives the ModelAttribute("postitNote")
+    // That attribute is the attribute that is mapped to the form, so here
+    // we can save the postit note because we get the data that was entered
+    // into the form.
+    // Notice the `method = RequestMethod.POST` part
+    @RequestMapping(value = "/day", method = RequestMethod.POST)
+    public String dayViewPost(@ModelAttribute("day") Day day,
+                                     Model model){
+
+        // Save the Postit Note that we received from the form
+        dayService.save(day);
+
+        // Here we get all the Postit Notes (in a reverse order) and add them to the model
+        model.addAttribute("days", dayService.findAllByOrderByIdDesc());
+
+        // Add a new Postit Note to the model for the form
+        // If you look at the form in PostitNotes.jsp, you can see that we
+        // reference this attribute there by the name `postitNote`.
+        model.addAttribute("day", new Day());
+
+        // Return the view
+        return "days/Days";
+    }
+
+    // Method that returns the correct view for the URL /postit/{name}
+    // The {name} part is a Path Variable, and we can reference that in our method
+    // parameters as a @PathVariable. This enables us to create dynamic URLs that are
+    // based on the data that we have.
+    // This method finds all Postit Notes posted by someone with the requested {name}
+    // and returns a list with all those Postit Notes.
+    @RequestMapping(value = "/day/{date}", method = RequestMethod.GET)
+    public String daysGetNotesFromDate(@PathVariable String date,
+                                             Model model){
+
+        // Get all Postit Notes with this name and add them to the model
+        model.addAttribute("days", dayService.findByDate(date));
+
+        // Add a new Postit Note to the model for the form
+        // If you look at the form in PostitNotes.jsp, you can see that we
+        // reference this attribute there by the name `postitNote`.
+        model.addAttribute("day", new Day());
+
+        // Return the view
+        return "days/Days";
+    }
 }
---
+++ src/main/java/project/persistence/repositories/DayRepository.java
@@ -0,0 +1,39 @@
+package project.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import project.persistence.entities.Day;
+
+import java.util.List;
+
+/**
+ * By extending the {@link JpaRepository} we have access to powerful methods.
+ * For detailed information, see:
+ * http://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
+ * http://docs.spring.io/spring-data/data-commons/docs/1.6.1.RELEASE/reference/html/repositories.html
+ *
+ */
+public interface DayRepository extends JpaRepository<Day, Long> {
+
+    Day save(Day day);
+
+    void delete(Day day);
+
+    List<Day> findAll();
+
+    // If we need a custom query that maybe doesn't fit the naming convention used by the JPA repository,
+    // then we can write it quite easily with the @Query notation, like you see below.
+    // This method returns all PostitNotes where the length of the name is equal or greater than 3 characters.
+  //  @Query(value = "SELECT p FROM day p where length(p.date) >= 3 ")
+   // List<Day> findAllWithNameLongerThan3Chars();
+
+    // Instead of the method findAllReverseOrder() in PostitNoteService.java,
+    // We could have used this method by adding the words
+    // ByOrderByIdDesc, which mean: Order By Id in a Descending order
+    //
+    List<Day> findAllByOrderByIdDesc();
+
+    Day findOne(Long id);
+
+    List<Day> findByDate(String date);
+}
