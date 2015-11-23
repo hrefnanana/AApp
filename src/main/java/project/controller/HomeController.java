adcffc0cc0fc2a905c62113d4dd4457dc4f4b527
Hrefna <hrefnanana@gmail.com>
2015-11-23 20:51:28 +0000

    longterm og útskráning

--- src/main/java/project/controller/HomeController.java
+++ src/main/java/project/controller/HomeController.java
@@ -12,19 +12,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import project.persistence.entities.User;
-import project.service.StringManipulationService;
+import project.service.*;
 
 @Controller
 @SessionAttributes({"user"})
 public class HomeController {
 
     // Instance Variables
-    StringManipulationService stringService;
+    UserService userService;
 
     // Dependency Injection
     @Autowired
-    public HomeController(StringManipulationService stringService) {
-        this.stringService = stringService;
+    public HomeController(UserService userService) {
+        this.userService = userService;
     }
 
     // Request mapping is the path that you want to map this method to
@@ -34,22 +34,37 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model){	
     	
-    	 LocalDate soberDate = LocalDate.of(2014, 6, 29);
-    	 LocalDate dateToday = LocalDate.now();
-    	 Period soberCounter = soberDate.until(dateToday);
-    	 int years = soberCounter.getYears();
- 		 int months = soberCounter.getMonths();
- 		 int days = soberCounter.getDays();
-    			 
-    	 model.addAttribute("years",years);
-    	 model.addAttribute("days",days);
-    	 model.addAttribute("months",months);
-    	 
+    	model.addAttribute("user", new User());
         // The string "Index" that is returned here is the name of the view
         // (the Index.jsp file) that is in the path /main/webapp/WEB-INF/jsp/
         // If you change "Index" to something else, be sure you have a .jsp
         // file that has the same name
-        return "Index";
+        return "logins/login";
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String homePost(@ModelAttribute("user") User user, Model model){	
+    	
+    	String userName = user.getUserName();
+    	String password = user.getPassword();
+    	
+    	User res = userService.findByUserNameAndPassword(userName, password);
+    	
+    	if(res != null) {
+
+    		model.addAttribute("user", res);
+    		String lastName = res.getLastName();
+        	System.out.println("seinna nafn " + lastName);
+        	return "redirect:/home";
+    	}
+    	
+    	else {
+    		
+    		String notLoggedIn = "Notandanafn og lykilorð stemma ekki"; 
+    		model.addAttribute("notLoggedIn", notLoggedIn);
+    		return "logins/login";
+    		
+    	} 
     }
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String loggedIn(@ModelAttribute("user") User user, Model model){	
@@ -79,35 +94,7 @@ public class HomeController {
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String user(Model model){
 
-        // Here we will show how to add attributes to a model and send it to the view
-
-        // Since this small example is for a user, let's create some attributes
-        // that users might usually have in a system
-        String name = "Rincewind";
-        String job  = "Wizzard";
-        String email = "rincewizz@unseenuni.edu";
-        String description = "most likely to survive in a dungeon dimension.";
-
-
-        // Since we want our attributes regarding the user always in the same format,
-        // we are going to convert some strings using our StringManipulationService
-
-        // Let's assume that the name, job and description always have
-        // the first character in upper case
-        name = stringService.convertsFirstCharInStringToUpperCase(name);
-        job = stringService.convertsFirstCharInStringToUpperCase(job);
-        description = stringService.convertsFirstCharInStringToUpperCase(description);
-
-        // Let's assume that we always want e-mail in lower case
-        email = stringService.convertStringToLowerCase(email);
-
-
-        // Now let's add the attributes to the model
-        model.addAttribute("name",name);
-        model.addAttribute("job",job);
-        model.addAttribute("email",email);
-        model.addAttribute("description",description);
-
+        
         // By adding attributes to the model, we can pass information from the controller
         // to the view (the .jsp file).
         // Look at the User.jsp file in /main/webapp/WEB-INF/jsp/ to see how the data is accessed
